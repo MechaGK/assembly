dc59ac2a59fd3f82d64e0d83aa69af8157c16593
Mads Grau Kristensen <madsgrau@gmail.com>
2017-09-07 15:25:45 +0200

    More silent script

--- run_imada.sh
+++ run_imada.sh
@@ -1,8 +1,8 @@
 filename="$(basename $1)"
 name="${filename%.*}"
 
-scp $1 imada:assembly/$filename
-ssh imada << HERE
+scp -q $1 imada:assembly/$filename
+ssh -T imada << HERE
     as assembly/${filename} -o assembly/objects/${name}.o
     ld assembly/objects/${name}.o -o assembly/output/${name}
     ./assembly/output/${name} 
